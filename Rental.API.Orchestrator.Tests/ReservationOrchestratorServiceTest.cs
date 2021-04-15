78024eff73a4d6a95157e1635596e5f4c03bf05a
Bernardo Tuaf <bmtuaf@gmail.com>
2021-04-14 21:27:34 -0300

    Adding test project for orchestrator api

---
+++ Rental.API.Orchestrator.Tests/ReservationOrchestratorServiceTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Rental.API.Orchestrator.Tests
+{
+    public class ReservationOrchestratorServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Rental.API.Orchestrator.Tests/SearchServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Rental.API.Orchestrator.Tests
+{
+    public class SearchServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
